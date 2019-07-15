e0d104fac83f1ae6ba24c2a202a96e505943c826
Jordan Mor <jordanmor@gmail.com>
2019-07-15 19:59:21 -0400

    Prompt user for guess and store the value

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,22 @@
 public class Game {
     private String answer;
+    private String hits;
+    private String misses;
 
     public Game(String answer) {
         this.answer = answer;
+        hits = "";
+        misses = "";
     }
+
+    public boolean applyGuess(char letter) {
+        boolean isHit = answer.indexOf(letter) != -1;
+        if(isHit) {
+            hits += letter;
+        } else {
+            misses += letter;
+        }
+        return isHit;
+    }
+
 }
--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,12 @@
 public class Hangman {
     public static void main(String[] args) {
-        Game game = new Game("hangman");
+        Game game = new Game("elephant");
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if(isHit) {
+            System.out.println("It's a hit!");
+        } else {
+            System.out.println("Oops missed.");
+        }
     }
 }
