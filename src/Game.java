f1038dc79ba410a66e5623978310a74f7cbc7132
Jordan Mor <jordanmor@gmail.com>
2019-07-16 10:14:33 -0400

    Validate and normalize user input

--- src/Game.java
+++ src/Game.java
@@ -5,12 +5,24 @@ public class Game {
     private String misses;
 
     public Game(String answer) {
-        this.answer = answer;
+        this.answer = answer.toLowerCase();
         hits = "";
         misses = "";
     }
 
+    private char normalizeGuess(char letter) {
+        if(!Character.isLetter(letter)) {
+            throw new IllegalArgumentException("A letter is required");
+        }
+        letter = Character.toLowerCase(letter);
+        if(hits.indexOf(letter) != -1 || misses.indexOf(letter) != -1) {
+            throw new IllegalArgumentException(letter + "has already been guessed");
+        }
+        return letter;
+    }
+
     public boolean applyGuess(char letter) {
+        letter = normalizeGuess(letter);
         boolean isHit = answer.indexOf(letter) != -1;
         if(isHit) {
             hits += letter;
