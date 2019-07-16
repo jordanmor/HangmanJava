0dbee15321934f5cebc36ac9470fdc8b157d0382
Jordan Mor <jordanmor@gmail.com>
2019-07-16 08:10:56 -0400

    Inform the user about the current state of the puzzle

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,11 +2,13 @@ public class Hangman {
     public static void main(String[] args) {
         Game game = new Game("elephant");
         Prompter prompter = new Prompter(game);
+        prompter.displayProgress();
         boolean isHit = prompter.promptForGuess();
         if(isHit) {
             System.out.println("It's a hit!");
         } else {
             System.out.println("Oops missed.");
         }
+        prompter.displayProgress();
     }
 }
